84197c1ab05b0812739a592ae70c29b76ce95130
Thibaud Selmire <t.selmire@gmail.com>
2017-11-07 12:14:24 +0100

    Ajax op, background de test

--- script/ajax.js
+++ script/ajax.js
@@ -1,37 +1,17 @@
-function getData(page) {
-  var xmlHttp = createXMLHttp(success, error);
-  xmlHttp.open('get', 'http://localhost/page/'+page+'.html', true);
-  xmlHttp.send(null);
-  xmlHttp.onreadystatechange = function() {
-    if (xmlHttp.readyState === 4) {
-      if (xmlHttp.status === 200) {
-        d = document.getElementById("content");
-        d.innerHTML = success.call(null, xmlHttp.responseText);
-      } else {
-        error.call(null, xmlHttp.responseText);
-      }
+function ajax (page){
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'page/'+page+'.html');
+xhr.send(null);
+xhr.onreadystatechange = function () {
+  var DONE = 4; // readyState 4 means the request is done.
+  var OK = 200; // status 200 is a successful return.
+  if (xhr.readyState === DONE) {
+    if (xhr.status === OK){
+      var r = xhr.responseText;
+      document.getElementById('content').innerHTML = r;
     } else {
-      //still processing
-    }
-  };
-}
-
-function createXMLHttp() {
-  //If XMLHttpRequest is available then using it
-  if (typeof XMLHttpRequest !== undefined) {
-    return new XMLHttpRequest;
-  //if window.ActiveXObject is available than the user is using IE...so we have to create the newest version XMLHttp object
-  } else if (window.ActiveXObject) {
-    var ieXMLHttpVersions = ['MSXML2.XMLHttp.5.0', 'MSXML2.XMLHttp.4.0', 'MSXML2.XMLHttp.3.0', 'MSXML2.XMLHttp', 'Microsoft.XMLHttp'],
-        xmlHttp;
-    //In this array we are starting from the first element (newest version) and trying to create it. If there is an
-    //exception thrown we are handling it (and doing nothing ^^)
-    for (var i = 0; i < ieXMLHttpVersions.length; i++) {
-      try {
-        xmlHttp = new ActiveXObject(ieXMLHttpVersions[i]);
-        return xmlHttp;
-      } catch (e) {
-      }
+      console.log('Error: ' + xhr.status); // An error occurred during the request.
     }
   }
 }
+};
