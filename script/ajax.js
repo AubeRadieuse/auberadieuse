5ed51e317597ef87d9710e0337985c715345af6a
Thibaud Selmire <t.selmire@gmail.com>
2017-11-07 11:04:26 +0100

    Commit

--- script/ajax.js
+++ script/ajax.js
@@ -15,3 +15,23 @@ function getData(page) {
     }
   };
 }
+
+function createXMLHttp() {
+  //If XMLHttpRequest is available then using it
+  if (typeof XMLHttpRequest !== undefined) {
+    return new XMLHttpRequest;
+  //if window.ActiveXObject is available than the user is using IE...so we have to create the newest version XMLHttp object
+  } else if (window.ActiveXObject) {
+    var ieXMLHttpVersions = ['MSXML2.XMLHttp.5.0', 'MSXML2.XMLHttp.4.0', 'MSXML2.XMLHttp.3.0', 'MSXML2.XMLHttp', 'Microsoft.XMLHttp'],
+        xmlHttp;
+    //In this array we are starting from the first element (newest version) and trying to create it. If there is an
+    //exception thrown we are handling it (and doing nothing ^^)
+    for (var i = 0; i < ieXMLHttpVersions.length; i++) {
+      try {
+        xmlHttp = new ActiveXObject(ieXMLHttpVersions[i]);
+        return xmlHttp;
+      } catch (e) {
+      }
+    }
+  }
+}
